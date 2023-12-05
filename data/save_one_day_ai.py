ec098a44c7029e2ac7712ea895c9568763b7ff0c
agape1225 <scg9268@gmail.com>
2023-12-05 18:00:02 +0900

    ✨ update gpt method
    using mongodb

--- data/save_one_day_ai.py
+++ data/save_one_day_ai.py
@@ -6,9 +6,9 @@ sys.path.append(os.path.realpath(__file__)[0:-18]+"..")
 from machine.bithumb_machine import BithumbMachine
 from db.mongodb.mongodb_handler import MongoDBHandler
 from AI.lstm_machine import LstmMachine
+from machine.chart_machine import ChartMachine
+from machine.chatGPT_machine import ChatMachine
 import datetime
-
-
 
 def extract_close_prices(data):
     close_prices = [float(entry['close_price']) for entry in data]
@@ -34,3 +34,14 @@ one_day_data = {}
 one_day_data["timestamp"] = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 one_day_data["predicted_price"] = result + 0.0
 mongodbMachine.insert_item(data=one_day_data, db_name="AI", collection_name="predicted_data")
+
+chart_machine = ChartMachine()
+chat_machine = ChatMachine()
+
+actual_data_str, predicted_data_str = chart_machine.get_analysis_chart()
+res = chat_machine.get_analysis_result(actual_data_str, predicted_data_str)
+
+analysis_data = {"gpt_response":res, "timestamp":datetime.date.today().strftime("%Y-%m-%d")}
+print(analysis_data)
+
+mongodbMachine.insert_item(data = analysis_data, db_name="AI", collection_name="analysis_data")
